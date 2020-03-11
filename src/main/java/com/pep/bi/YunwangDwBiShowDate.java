42756406a8a9a8f991a4dac1d518ef32b75e2a26
zhangfz <smile46227@163.com>
2020-03-11 20:15:44 +0800

    提交EMR搭建 修改队列dwq

--- src/main/java/com/pep/bi/YunwangDwBiShowDate.java
+++ src/main/java/com/pep/bi/YunwangDwBiShowDate.java
@@ -1,5 +1,7 @@
 package com.pep.bi;
 
+import com.pep.common.DbProperties;
+
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,30 +16,27 @@ import java.util.Properties;
  * @Description:
  * @Date: 2019/7/25
  */
-public class YuwangDwBiShowDate {
+public class YunwangDwBiShowDate {
     public static void main(String[] args) {
 
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         calendar.add(Calendar.DATE,-1);
-        String yestodayStr = format.format(calendar.getTime());
+        String yesterdayStr = format.format(calendar.getTime());
 
         String insertSql = "insert into show.bi_show (bi_show_time) VALUES (?); ";
 
         //读取数据源文件
-        InputStream input = YuwangDwBiShowDate.class.getClassLoader().getResourceAsStream("datasource.properties");
-        Properties prop = new Properties();
+        Properties prop = DbProperties.propScp;
         try {
-            prop.load(input);
-            String username = prop.getProperty("username");
+            String username = prop.getProperty("user");
             String password = prop.getProperty("password");
             String url = prop.getProperty("url");
             Connection conn = DriverManager.getConnection(url,username,password);
             PreparedStatement prepStat = conn.prepareStatement(insertSql);
-            prepStat.setString(1,yestodayStr);
+            prepStat.setString(1,yesterdayStr);
             boolean res = prepStat.execute();
-            input.close();
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
